02a7f7825e26446ec4905ecadf1ed5e354199f61
pasha <shaik.pasha@adap.tv>
2014-10-31 00:20:05 +0530

    Update 2014-10-30T18:50:04.796Z

--- js/dashboard.js
+++ js/dashboard.js
@@ -142,7 +142,30 @@ function prFrame () {
     return directive;
 
     function link(scope, element, attrs) {
-
+        function getDocHeight(doc) {
+            doc = doc || document;
+            // stackoverflow.com/questions/1145850/
+            var body = doc.body, html = doc.documentElement;
+            var height = Math.max( body.scrollHeight, body.offsetHeight,
+                html.clientHeight, html.scrollHeight, html.offsetHeight );
+            return height;
+        }
+        function setIframeHeight() {
+            var ifrm = element[0];
+            var doc = ifrm.contentDocument? ifrm.contentDocument:
+                ifrm.contentWindow.document;
+            ifrm.style.visibility = 'hidden';
+            ifrm.style.height = "10px"; // reset to minimal height ...
+            // IE opt. for bing/msn needs a bit added or scrollbar appears
+            ifrm.style.height = getDocHeight( doc ) + 4 + "px";
+            ifrm.style.visibility = 'visible';
+            console.log("height is",ifrm.style.height);
+        }
+        scope.$watch('projectUrl',function(newVal){
+            if(newVal){
+                setIframeHeight();
+            }
+        })
     }
 };
 
